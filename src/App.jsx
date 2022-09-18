b4720f9306837fbe80679090cdac3a8c3699d930
nyascla <llorenslavernia123@gmail.com>
2022-09-18 20:15:41 +0200

    proxy

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
+  
   return (
-       
-    fetch('http://example.com/movies.json')
-      .then(response => response.json())
-      .then(data => console.log(data))  
+     
+    <>
+      hola
+    </>
+
 
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+fetch('/api')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
